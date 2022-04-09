0b316db36fff019666772deef67f711d14107c7e
akon <chenbin666666@126.com>
2022-04-09 17:50:24 +0800

    update

--- src/main/java/com/akon/springboot/mybatis/utils/CollectionsUtil.java
+++ src/main/java/com/akon/springboot/mybatis/utils/CollectionsUtil.java
@@ -1,8 +1,17 @@
 package com.akon.springboot.mybatis.utils;
 
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CollectionsUtil {
+    public static void main(String[] args) {
+        List list=new ArrayList();
+        boolean empty = CollectionUtils.isEmpty(list);
+        System.out.println(empty);
+    }
 
     public  static <T> boolean  isEmpty(List<T> list){
         if(list==null||list.isEmpty()){
